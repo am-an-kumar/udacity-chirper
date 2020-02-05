61bd64c4795faae5eb290e423cdf16786318e75b
Aman <amankumar.edith@gmail.com>
2020-02-05 19:46:47 +0530

    Added navbar and handled routing for / and /new

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
@@ -6,6 +7,7 @@ import LoadingBar from 'react-redux-loading'
 import Dashboard from './Dashboard'
 import NewTweet from './NewTweet'
 import TweetPage from './TweetPage'
+import Nav from './Nav'
 
 class App extends Component {
   componentDidMount() {
@@ -14,11 +16,21 @@ class App extends Component {
 
   render() {
     return (
-      <>
-        <LoadingBar />
-        {!this.props.loading && <TweetPage tweetId='czpa59mg577x1oo45cup0d' />}
-        {/* <NewTweet /> */}
-      </>
+      <Router>
+        <>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            {!this.props.loading && (
+              <div>
+                <Route exact path='/' component={Dashboard} />
+                <Route path='/new' component={NewTweet} />
+                <Route path='tweet/:id' component={TweetPage} />
+              </div>
+            )}
+          </div>
+        </>
+      </Router>
     )
   }
 }
